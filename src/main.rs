a4377fbca7cbfc3b8d99db85530c59bc9df143a0
Keith Raymond <raymondke99@gmail.com>
2018-02-05 21:23:02 -1000

    Handle peerDependencies missing from package.json

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ extern crate serde_json;
 use serde_json::Value;
 
 use std::fs::File;
-use std::env;
 use std::io::Read;
 use std::process::Command;
 
@@ -15,24 +14,6 @@ use termion::color;
 use regex::Regex;
 
 fn main() {
-  println!("{cyan}========================================{reset}",
-    cyan  = color::Fg(color::Cyan),
-    reset = color::Fg(color::Reset)
-  );
-  println!("{cyan}|     Installing Peer Dependencies     |{reset}",
-    cyan  = color::Fg(color::Cyan),
-    reset = color::Fg(color::Reset)
-  );
-  println!("{cyan}========================================{reset}",
-    cyan  = color::Fg(color::Cyan),
-    reset = color::Fg(color::Reset)
-  );
-
-  let current_directory = env::current_dir()
-    .expect("current directory is wrong")
-    .display().to_string();
-
-
   let mut file = match File::open("package.json") {
     Err(why) => panic!("couldn't read package.json: {:?}", why),
     Ok(file) => file,
@@ -45,56 +26,68 @@ fn main() {
     .expect("serde_json what? from what?");
   let mut packages = vec![];
 
-  for obj in json["peerDependencies"].as_object()
-    .expect("as_object is all fucked up")
-  {
-    let re    = Regex::new(r####"""####)
-      .expect("re Regex::new not working");
-    let no_at = Regex::new(r"\^")
-      .expect("no_at Regex::new not working");
+  match json["peerDependencies"].as_object() {
+    Some(peer_dependencies) => {
+      print_block("Installing Peer Dependencies");
 
-    let v1 = obj.1.to_string();
-    let v2 = re.replace_all(&v1, "");
-    let version = no_at.replace_all(&v2, "@");
+      for obj in peer_dependencies {
+        let re    = Regex::new(r####"""####)
+          .expect("re Regex::new not working");
+        let no_at = Regex::new(r"\^")
+          .expect("no_at Regex::new not working");
 
-    packages.push(format!("{}{}", obj.0, version));
+        let v1 = obj.1.to_string();
+        let v2 = re.replace_all(&v1, "");
+        let version = no_at.replace_all(&v2, "@");
+
+        packages.push(format!("{}{}", obj.0, version));
+      }
+      println!("npm install {}", packages.iter().cloned().collect::<String>());
+      let args = &packages[..];
+
+      Command::new("npm")
+        .arg("install")
+        .args(args)
+        .status()
+        .expect("npm install failed you dumb bastard");
+    },
+    None => {
+      print_block("No peerDependencies found");
+    }
+  };
+
+  print_block("Installing Dependencies");
+
+  Command::new("npm")
+    .arg("install")
+    .status()
+    .expect("npm install failed you dumb bastard");
+
+  print_block("Done :)");
+}
+
+fn print_block(msg: &str) {
+  let eq_len  = 40;
+  let msg_len = msg.len();
+
+  let space_around = (eq_len - msg_len) / 2;
+  let mut prepend = String::new();
+  let mut append  = String::new();
+  for i in 1..space_around {
+    prepend += " ";
+    append  += " ";
   }
-  println!("npm install {}", packages.iter().cloned().collect::<String>());
-  let args = &packages[..];
-
-  Command::new("cd")
-    .arg(current_directory)
-    .status()
-    .expect("npm install failed you dumb bastard");
-  // Command::new("pwd")
-  //   .status()
-  //   .expect("npm install failed you dumb bastard");
-
-  Command::new("npm")
-    .arg("install")
-    .args(args)
-    .status()
-    .expect("npm install failed you dumb bastard");
 
   println!("{cyan}========================================{reset}",
     cyan  = color::Fg(color::Cyan),
     reset = color::Fg(color::Reset)
   );
-  println!("{cyan}|         Installing Dependencies      |{reset}",
+  println!("{cyan}|{message}|{reset}",
     cyan  = color::Fg(color::Cyan),
-    reset = color::Fg(color::Reset)
+    reset = color::Fg(color::Reset),
+    message = format!("{}{}{}", prepend, msg, append)
   );
   println!("{cyan}========================================{reset}",
-    cyan  = color::Fg(color::Cyan),
-    reset = color::Fg(color::Reset)
-  );
-
-  Command::new("npm")
-    .arg("install")
-    .status()
-    .expect("npm install failed you dumb bastard");
-
-  println!("{cyan}=============  DONE :) ================{reset}",
     cyan  = color::Fg(color::Cyan),
     reset = color::Fg(color::Reset)
   );
